e553abb3d504da4ccf3032e44ae81dcf80ca75b5
Dubbyw <106718081+DUBBEP@users.noreply.github.com>
2024-11-07 11:33:58 -0700

    Project Added

---
+++ SRPNPC Project/Assets/Scripts/NPC.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    internal void TakeDamage(int amount)
+    {
+        GetComponent<IHealth>().TakeDamage(amount);
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * 3 * Time.deltaTime;
+        transform.Rotate(0f, 90 * Time.deltaTime, 0f);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10);
+        }
+    }
+}
